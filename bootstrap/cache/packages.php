a960da0679b71d067917dd5f829fdae121e6255a
cbf <906607626@qq.com>
2019-08-12 19:28:17 +0800

    init

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,81 @@
+<?php return array (
+  'dingo/api' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Dingo\\Api\\Provider\\LaravelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'API' => 'Dingo\\Api\\Facade\\API',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/nexmo-notification-channel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Notifications\\NexmoChannelServiceProvider',
+    ),
+  ),
+  'laravel/slack-notification-channel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Notifications\\SlackChannelServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'liyu/dingo-serializer-switch' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Liyu\\Dingo\\ServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'orangehill/iseed' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Orangehill\\Iseed\\IseedServiceProvider',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+  'tymon/jwt-auth' => 
+  array (
+    'aliases' => 
+    array (
+      'JWTAuth' => 'Tymon\\JWTAuth\\Facades\\JWTAuth',
+      'JWTFactory' => 'Tymon\\JWTAuth\\Facades\\JWTFactory',
+    ),
+    'providers' => 
+    array (
+      0 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
+    ),
+  ),
+);
---
+++ storage/framework/views/caece3e9144095cb463cf50f3c186ad2a1ec2217.php
@@ -0,0 +1,12 @@
+<h4>
+    修改邮箱验证码
+</h4>
+<p>
+    你的验证码为 <?php echo e($captcha); ?>, 有效期为10分钟
+</p>
+
+<br>
+<br>
+<br>
+
+<small style="text-align:right">百步梯人员管理中心</small>
