4248cfee14b35bd044fd879edcf1a683857ef6a1
KanoLin <fordreams@outlook.com>
2019-09-09 18:02:57 +0800

    update factory

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -4,9 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Schedule::class, function (Faker $faker) {
     return [
-        'user_id'=>mt_rand(1930,2130),
+        'user_id'=>mt_rand(1930,2500),
         'week'=>1,
         'day'=>2,
-        'class'=>mt_rand(1,4),
+        'class'=>mt_rand(1,8),
     ];
 });
